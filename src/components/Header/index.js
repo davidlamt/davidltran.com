60853ac9a9eb50e7f224456ff8e2b8e7652c7017
David Tran <davidlamt@gmail.com>
2018-03-25 11:26:30 -0700

    Styled main header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  background: #fff;
+  margin: 2rem 0 1.45rem 0;
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+
+  h1 {
+    margin: 0;
+
+    a {
+      color: #333;
+      text-decoration: none;
+    }
+  }
+`;
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          David Tran
-        </Link>
+  <HeaderWrapper>
+    <HeaderContainer>
+      <h1>
+        <Link to="/">David Tran</Link>
       </h1>
-    </div>
-  </div>
+    </HeaderContainer>
+  </HeaderWrapper>
 );
 
 export default Header;
