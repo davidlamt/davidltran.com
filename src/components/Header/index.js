fbc5abfc7248819d099fa4337865983e7c36ecd2
David Tran <davidlamt@gmail.com>
2018-03-25 10:56:25 -0700

    Switch line endings from CRLF to LF

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+        background: 'rebeccapurple',
+        marginBottom: '1.45rem',
     }}
   >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '1.45rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+              color: 'white',
+              textDecoration: 'none',
           }}
         >
           Gatsby
@@ -28,6 +28,6 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -17,19 +17,19 @@ const TemplateWrapper = ({ children }) => (
     <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+    children: PropTypes.func,
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
