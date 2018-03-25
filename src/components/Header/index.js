364d80e9abb34a29e425b6ca6aec8425d78bd584
David Tran <davidlamt@gmail.com>
2018-03-25 11:06:04 -0700

    Add styled-components package

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,26 +4,26 @@ import Link from 'gatsby-link';
 const Header = () => (
   <div
     style={{
-        background: 'rebeccapurple',
-        marginBottom: '1.45rem',
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
     }}
   >
     <div
       style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '1.45rem 1.0875rem',
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-              color: 'white',
-              textDecoration: 'none',
+            color: 'white',
+            textDecoration: 'none',
           }}
         >
-          Gatsby
+          David Tran
         </Link>
       </h1>
     </div>
