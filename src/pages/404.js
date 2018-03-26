821465289319345d9ff9167362dfedbbe940490c
David Tran <davidlamt@gmail.com>
2018-03-25 17:05:38 -0700

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Page Not Found</h1>
+    <p>Looks like you have followed a broken link or entered a URL that does not exist on this site.</p>
+    <Link to="/">Take me back to the home page.</Link>
   </div>
 );
 
