af4b7c89b8a2867a2e13e30ae074b48d51158944
David Tran <davidlamt@gmail.com>
2018-03-25 17:25:50 -0700

    Create Post component

---
+++ src/components/Post/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class PostPage extends Component {
+  render() {
+    const { data } = this.props;
+
+    console.log(data);
+
+    return (
+      <div>
+        <span>{data.markdownRemark.frontmatter.date}</span>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: data.markdownRemark.html,
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+      html
+    }
+  }
+`;
