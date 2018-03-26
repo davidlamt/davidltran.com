4f2fc5b7ed55dd533f62dae7a4847525220b8d1a
David Tran <davidlamt@gmail.com>
2018-03-25 21:02:33 -0700

    Update max markdown image size

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 590,
+              maxWidth: 680,
             },
           },
         ],
