90503e87c0d5228b5f8e0480aaa2b08b8893874e
David Tran <davidlamt@gmail.com>
2018-07-15 19:29:08 -0700

    Update styles for `PostListing` component

--- src/components/PostListing/index.js
+++ src/components/PostListing/index.js
@@ -8,6 +8,20 @@ const PostListingContainer = styled.li`
     color: #aaa;
     margin-right: 1rem;
   }
+
+  a {
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    box-shadow: 0 2px 0px 0px #cee6f9;
+    color: #333;
+    text-decoration: none;
+    border-bottom: 1px solid #cee6f9;
+    padding-top: 2px;
+
+    :active,
+    :hover {
+      background: #cee6f9;
+    }
+  }
 `;
 
 const PostListing = ({ slug, title, date }) => (
