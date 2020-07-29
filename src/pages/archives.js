dbd39058e4eac7f227a136ed05d09a9d3e9fed27
David Tran <davidlamt@gmail.com>
2020-07-28 20:55:47 -0700

    Removing year grouping from archives page

--- src/pages/archives.js
+++ src/pages/archives.js
@@ -1,117 +1,12 @@
-import React, { Component } from 'react';
-import { graphql } from 'gatsby';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
+import React from 'react';
 
 import Layout from '../components/Layout';
+import Posts from '../components/Posts';
 
-import PostListing from '../components/PostListing';
+const Archives = () => (
+  <Layout>
+    <Posts />
+  </Layout>
+);
 
-const ArchivesPage = styled.div`
-  ul {
-    list-style: none;
-    margin-left: 0;
-  }
-
-  .group {
-    margin-bottom: 3rem;
-  }
-`;
-
-export default class Archives extends Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      allMarkdownRemark: PropTypes.shape({
-        edges: PropTypes.arrayOf(
-          PropTypes.shape({
-            node: PropTypes.shape({
-              id: PropTypes.string,
-              fields: PropTypes.shape({
-                slug: PropTypes.string,
-              }),
-              frontmatter: PropTypes.shape({
-                title: PropTypes.string,
-                date: PropTypes.string,
-              }),
-            }),
-          })
-        ),
-      }),
-    }),
-  };
-
-  static defaultProps = {
-    data: {
-      allMarkdownRemark: {
-        edges: [
-          {
-            node: {
-              id: 'Default',
-              fields: {
-                slug: '/default/',
-              },
-              frontmatter: {
-                title: 'Default Title',
-                date: '1970-01-01',
-              },
-            },
-          },
-        ],
-      },
-    },
-  };
-
-  render() {
-    const { data } = this.props;
-
-    const groups = _.groupBy(data.allMarkdownRemark.edges, ({ node }) =>
-      new Date(node.frontmatter.date).getFullYear()
-    );
-    const years = Object.keys(groups).sort((a, b) => b - a);
-
-    return (
-      <Layout>
-        <ArchivesPage>
-          <h1>Archives</h1>
-          {_.map(years, year => (
-            <div className="group" key={year}>
-              <h2>{year}</h2>
-              <ul>
-                {groups[year].map(({ node: post }) => (
-                  <PostListing
-                    key={post.id}
-                    slug={post.fields.slug}
-                    title={post.frontmatter.title}
-                    date={post.frontmatter.date}
-                    tags={post.frontmatter.tags}
-                  />
-                ))}
-              </ul>
-            </div>
-          ))}
-        </ArchivesPage>
-      </Layout>
-    );
-  }
-}
-
-export const query = graphql`
-  query ArchivesQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-            tags
-          }
-        }
-      }
-    }
-  }
-`;
+export default Archives;
