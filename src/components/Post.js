dc89c0a0130356f49e65e666c525702394534dc7
David Tran <davidlamt@gmail.com>
2020-07-30 21:56:50 -0700

    Update post metadata design

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,15 +4,12 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import Layout from './Layout';
+import { Tag } from './';
 
 const BASE_REPO_URL =
   'https://github.com/davidlamt/davidtranscend-com-gatsby/blob/master/src/posts';
 
 const PostContainer = styled.div`
-  h1 {
-    margin-bottom: 0.5rem;
-  }
-
   a.gatsby-resp-image-link {
     border-bottom: none;
   }
@@ -20,31 +17,18 @@ const PostContainer = styled.div`
 
 const PostMeta = styled.div`
   margin-bottom: 1.5rem;
-  display: flex;
-  flex-direction: row;
+`;
 
-  span.date {
-    margin-right: 0.5rem;
-    padding-right: 0.5rem;
-    border-right: 1px solid #333;
-  }
-
-  svg {
-    margin-top: 0.2rem;
-    margin-right: 0.5rem;
-  }
-
-  ul {
-    margin: 0;
-    list-style: none;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-
-    li {
-      margin: 0 0.5rem 0 0;
-    }
-  }
+const PublishInfo = styled.div`
+  background-color: #384858;
+  border-bottom-right-radius: 25px;
+  border-top-right-radius: 25px;
+  color: #fff;
+  display: inline-block;
+  margin-bottom: 0.5rem;
+  margin-left: -60px;
+  padding-left: 60px;
+  padding-right: 20px;
 `;
 
 const PostMarkdown = styled.div`
@@ -85,14 +69,9 @@ export default class Post extends Component {
     },
   };
 
-  generateTag = tag => (
-    <li key={tag}>
-      <Link to={`/tags/${tag}`}>#{tag}</Link>
-    </li>
-  );
-
   render() {
     const { data } = this.props;
+    const { date, tags, title } = data.markdownRemark.frontmatter;
     const fileRelativePath = data.markdownRemark.fileAbsolutePath
       .split('/')
       .slice(-2)
@@ -102,22 +81,14 @@ export default class Post extends Component {
     return (
       <Layout>
         <PostContainer>
-          <h1>{data.markdownRemark.frontmatter.title}</h1>
+          <h1>{title}</h1>
           <PostMeta>
-            <span className="date">{data.markdownRemark.frontmatter.date}</span>
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M576 448q0-53-37.5-90.5t-90.5-37.5-90.5 37.5-37.5 90.5 37.5 90.5 90.5 37.5 90.5-37.5 37.5-90.5zm1067 576q0 53-37 90l-491 492q-39 37-91 37-53 0-90-37l-715-716q-38-37-64.5-101t-26.5-117v-416q0-52 38-90t90-38h416q53 0 117 26.5t102 64.5l715 714q37 39 37 91z" />
-            </svg>
-            <ul>
-              {data.markdownRemark.frontmatter.tags.map(tag =>
-                this.generateTag(tag)
-              )}
-            </ul>
+            <PublishInfo>Published on {date}</PublishInfo>
+            <div>
+              {tags.map(tag => (
+                <Tag tagName={tag} key={tag} />
+              ))}
+            </div>
           </PostMeta>
           <PostMarkdown
             // eslint-disable-next-line react/no-danger
