1206591704a89a9bf3eda150e23a6685b13b5244
David Tran <davidlamt@gmail.com>
2020-08-06 21:10:04 -0700

    Display Project details

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Project = ({ project }) => <div>{project.title}</div>;
+import { Tag } from './';
+
+const ProjectContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 500px;
+`;
+
+const InfoContainer = styled.div`
+  text-align: center;
+`;
+
+const ProjectTitle = styled.p`
+  font-size: 1.5em;
+  font-weight: 600;
+  margin-bottom: 0;
+  margin-top: 20px;
+`;
+
+const ProjectDescription = styled.p`
+  margin-bottom: 0;
+`;
+
+const Link = styled.a`
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const Project = ({ project }) => {
+  const { alt, description, image, links, tags, title } = project;
+
+  return (
+    <ProjectContainer>
+      <Image src={image} alt={alt}></Image>
+      <InfoContainer>
+        {tags.map(tag => (
+          <Tag key={tag} tagName={tag} />
+        ))}
+        <ProjectTitle>{title}</ProjectTitle>
+        <ProjectDescription>{description}</ProjectDescription>
+        {links.map(link => (
+          <Link href={link.url} key={link.url}>
+            {link.name}
+          </Link>
+        ))}
+      </InfoContainer>
+    </ProjectContainer>
+  );
+};
 
 Project.propTypes = {
   project: PropTypes.shape({
