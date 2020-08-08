d00dbf64ac11bf6a02a4b501b2ed8df28556736c
David Tran <davidlamt@gmail.com>
2020-08-08 13:02:32 -0700

    Add base experience page

---
+++ src/pages/experience.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const Experience = () => (
+  <Layout>
+    <h1>Experience</h1>
+    <p>
+      For more details, check out my{' '}
+      <a href="https://www.linkedin.com/in/davidlamt/">LinkedIn</a>.
+    </p>
+  </Layout>
+);
+
+export default Experience;
