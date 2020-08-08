05186e7cb8877fea4387309b1c84e67705b8632f
David Tran <davidlamt@gmail.com>
2020-08-07 21:50:39 -0700

    Add more projects

--- src/pages/projects/projectsData.js
+++ src/pages/projects/projectsData.js
@@ -1,5 +1,40 @@
 const projectsData = [
   {
+    title: 'The Tail End Visualizer',
+    description: 'A visualization tool for how much time you have left.',
+    image: 'tail-end-visualizer.png',
+    alt: 'Screenshot of The The Tail End Visualizer application.',
+    tags: ['react', 'typescript'],
+    links: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/davidlamt/tail-end-visualizer',
+      },
+      {
+        name: 'Live',
+        url: 'https://davidlamt.github.io/tail-end-visualizer/',
+      },
+    ],
+  },
+  {
+    title: 'The True Cost',
+    description:
+      'Tool for you to lament the true cost of common monthly expenses.',
+    image: 'the-true-cost.png',
+    alt: 'Screenshot of The True Cost application.',
+    tags: ['react', 'typescript'],
+    links: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/davidlamt/the-true-cost',
+      },
+      {
+        name: 'Live',
+        url: 'https://davidlamt.github.io/the-true-cost/',
+      },
+    ],
+  },
+  {
     title: 'AntFinder',
     description:
       'An organized marketplace for UCI students to advertise and purchase various goods.',
