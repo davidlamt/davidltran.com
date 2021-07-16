c1e66076d9327a7cae42c4e9283b4214947180ad
David Tran <davidlamt@users.noreply.github.com>
2021-07-15 21:19:57 -0700

    Update tmux workspace script

--- tmux-workspace.sh
+++ tmux-workspace.sh
@@ -7,20 +7,18 @@ session="davidltran-com-workspace"
 tmux has-session -t $session 2>/dev/null
 
 if [ $? != 0 ]; then
-  # Create new detached session and start vim
-  tmux new-session -d -n "develop" -s $session -x $(tput cols) -y $(tput lines)
-  tmux send-keys "vim" C-m
-
-  # Gatsby server pane
-  tmux split-window -h -p 25
+  # Create new detached session with server window
+  tmux new-session -d -n "server" -s $session -x $(tput cols) -y $(tput lines)
   tmux send-keys "npm i && gatsby develop" C-m
 
-  # Git pane
-  tmux split-window -v
-  tmux send-keys "git fetch origin" C-m
+  # Git window
+  tmux new-window -n "git"
+  tmux send-keys "ssh-add ~/.ssh/id_rsa" C-m
 
-  # Select first pane
-  tmux select-pane -t 0
+  # vim window
+  tmux new-window -n "vim"
+  tmux send-keys "vim" C-m
+
 fi
 
 # Attach to created session
